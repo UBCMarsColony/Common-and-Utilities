5280b503522c8527f0c477e0a97d8aa78907bc57
hardik-ddod <hardik.singhi@alumni.ubc.ca>
2019-06-15 15:03:43 -0700

    Updated classDefinitions.py
    Making Function() an abstract class.

--- src/model/classDefinitions.py
+++ src/model/classDefinitions.py
@@ -1,20 +1,22 @@
+from abc import ABC, abstractmethod
+
 class Sphere():
     """Sphere object represents a sphere of influence;
        may be a planet, spaceship, asteroid ,etc."""
 
-       def __init__(self, modSys, spatialVector, functionVector, timeStep):
-           self.modSys = modSys
-           self.spatialVector = spatialVector
-           self.functionVector = functionVector
-           self.timeStep = timeStep
+    def __init__(self, modSys, spatialVector, functionVector, timeStep):
+        self.modSys = modSys
+        self.spatialVector = spatialVector
+        self.functionVector = functionVector
+        self.timeStep = timeStep
 
 class ModulatorSystem():
     """ModSys object is a graph or structured
        connection made up of Modulator objects
        and the links connecting them."""
 
-       def __init__(self, collectionOfModulators):
-           self.collectionOfModulators = collectionOfModulators
+    def __init__(self, collectionOfModulators):
+        self.collectionOfModulators = collectionOfModulators
 
 class Modulator():
     """Modulator object takes in an input (mass,
@@ -22,11 +24,16 @@ class Modulator():
        something. Uses function object to do
        conversions."""
 
-       def __init__(self, channels, function):
-           self.channels = channels
-           self.function = function
+    def __init__(self, channels, function):
+        self.channels = channels
+        self.function = function
+
 
 class Function():
-    """Function object converts an input into an
+    """Function is an abstract base class for other
+       types of function classes such as polynomial,
+       exponential, sinusoidal, logarithmic, custom,
+       etc.
+       Function object converts an input into an
        output; represents the relationship
        between the two."""
