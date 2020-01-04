33e0c3af2ea1f0a95d5056f2d1a2758b431e9d02
OmarAliQazi <omaraliqazi@outlook.com>
2020-01-04 12:57:10 -0800

    update

--- sensors/k-type thermocouple/Sabatier-Mission-Architecture_k-type-Thermocouple/Sabatier-Mission-Architecture_k-type-Thermocouple.ino
+++ sensors/k-type thermocouple/Sabatier-Mission-Architecture_k-type-Thermocouple/Sabatier-Mission-Architecture_k-type-Thermocouple.ino
@@ -2,31 +2,32 @@
 
 #include "max6675.h"
 //Thermocouple 1
-int thermoDO1 = 10;            //SO pin on HW-550 board
-int thermoCS1 = 11;            //CS pin (PWM)
-int thermoCLK1 = 12;           //SCK pin (PWM)
+int thermoDO1 = 7;            //SO pin on HW-550 board
+int thermoCS1 = 4;            //CS pin (PWM)
+int thermoCLK1 = 13;          //SCK pin (PWM)
 
 //Thermocouple 2
-int thermoDO2 = 3;            //SO pin on HW-550 board
-int thermoCS2 = 4;            //CS pin (PWM)
+int thermoDO2 = 6;            //SO pin on HW-550 board
+int thermoCS2 = 3;            //CS pin (PWM)
 //No initialization of thermocouple 2 clk because using common clk
 
+//Thermocouple 3
+int thermoDO3 = 5;            //SO pin on HW-550 board
+int thermoCS3 = 2;            //CS pin (PWM)
+//No initialization of thermocouple 3 clk because using common clk
+
 MAX6675 thermocouple1(thermoCLK1, thermoCS1, thermoDO1);      //Pin definitions
-int vccPin1 = 13;                // choose pin for vcc
 
 MAX6675 thermocouple2(thermoCLK1, thermoCS2, thermoDO2);      //Common clock for both thermocouples
-int vccPin2 = 6;                // choose pin for vcc
-int gndPin2 = 7;                // choose pin for gnd
+
+MAX6675 thermocouple3(thermoCLK1, thermoCS3, thermoDO3);      //Common clock for both thermocouples
 
 void setup() {
   Serial.begin(9600);
   // use Arduino pins 
-  pinMode(vccPin1, OUTPUT); digitalWrite(vccPin1, HIGH); //set pin as a 5V vcc
+  //Using vcc pin on board, no need to set a pin high
   //Using ground pin on board, no need to set a pin low
-  
-  pinMode(vccPin2, OUTPUT); digitalWrite(vccPin2, HIGH); //set pin as a 5V vcc
-  pinMode(gndPin2, OUTPUT); digitalWrite(gndPin2, LOW);  //set pin as a gnd
-  
+
   // wait for MAX chip to stabilize
   delay(500);
 }
@@ -34,10 +35,12 @@ void setup() {
 void loop() {
   // basic readout test, just print the current temp
   
-   Serial.print("Thermocouple 1 C = "); 
+   Serial.print("\n Thermocouple 1 C = "); 
    Serial.println(thermocouple1.readCelsius());
-   Serial.print("Thermocouple 2 C = ");
+   Serial.print("\n Thermocouple 2 C = ");
    Serial.println(thermocouple2.readCelsius());
+   Serial.print("\n Thermocouple 3 C = ");
+   Serial.println(thermocouple3.readCelsius());
  
    delay(1000);
 }
